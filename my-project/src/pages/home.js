a5200f819725057f4379f08e5777263daee57eae
Agathiyakumar <harishagathi@gmail.com>
2024-02-03 12:35:08 +0530

    first commit

--- my-project/src/pages/home.js
+++ my-project/src/pages/home.js
@@ -1,19 +1,21 @@
-import React from "react";
-
-
+import React from 'react';
+import MyFunctionalComponent from './slick';
 import 'bootstrap/dist/css/bootstrap.css';
 import { GoArrowDown } from "react-icons/go";
 
 
 const Home =()=>{
 
+
     return (
         <div className="Home">
           <div className="welcome">
    
-   <h1 className="wel">Welcome</h1>
+   <h1 className="wel" >Welcome</h1>
   
-   <h3 className="wel1">Funstation games are here to make your gaming experience high<br></br>You can have the games and their details</h3>
+   <h3 className="wel1" data-aos="fade-right"
+    data-aos-easing="linear"
+    data-aos-duration="1500">Funstation games are here to make your gaming experience high<br></br>You can have the games and their details</h3>
  
    </div>
    
@@ -24,32 +26,25 @@ const Home =()=>{
         <h1 className="mostex1">Most Exciting Game Infos</h1>
       </div>
 <div className="description">
-  <p className="description1"><h4>"Dive into a gaming haven at Funstation Games, where every click unlocks a <br></br>world of reviews, news, and exclusive insights. Immerse yourself in captivating <br></br>feature stories and join a vibrant community of like-minded gamers.</h4></p>
+  <p className="description1" data-aos="fade-left"
+    data-aos-easing="linear"
+    data-aos-duration="800"><h4>"Dive into a gaming haven at Funstation Games, where every click unlocks a <br></br>world of reviews, news, and exclusive insights. Immerse yourself in captivating <br></br>feature stories and join a vibrant community of like-minded gamers.</h4></p>
 </div>
 <div>
     <h1 className="ehead">Esports</h1>
   </div>
-<div className="parent">
-<h1 className="esporthead">
-   >>>  Join the Esport's  >>>
-  </h1>
-  <h5 className="esporthead1"> Join our game website for a thrilling gaming experience, featuring competitive leagues, engaging tournaments, and a vibrant community. Level up your skills and connect with fellow gamers on our platform today!"</h5>
-  <div className="esport">
-  
-      <img className="esportimg" src={"https://c4.wallpaperflare.com/wallpaper/608/212/2/electronic-sports-league-esl-esport-esports-wallpaper-preview.jpg"} alt=".." width="100%" height="auto"/>
-  </div>
-  <div className="esport">
-  <button class="button-53" role="button">Updates</button>
-  <div className="esport">
-  <button class="button-52" role="button">Upcoming</button>
-  </div>
-  </div>
+
+      
+      
+
+
+<div>
+
 </div>
-<h1>hello</h1>
+<div>
+  <MyFunctionalComponent/>
+</div>
 
-
-
- 
  
 </div>
 
